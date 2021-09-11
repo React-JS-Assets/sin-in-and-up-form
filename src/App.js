822c57f9f740026ec420128cdd7b0f1d1df34aaa
Zeeshan <zeeshanahmedd0010@gmail.com>
2021-09-11 23:53:15 +0500

    Updated handler methods

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import { Card, CardContent, Divider, Grid } from '@material-ui/core';
+import React,{useState} from 'react';
+import { Headings } from './Support/Headings/Headings';
+import { SimpleTextField } from './Support/TextFields/TextFields';
+import {lightBorder} from './Theme/borders'
+import {SimpleButton} from './Support/Buttons/Buttons'
+import { SimpleLinks } from './Support/Link/Links';
+import SinInForm from './Components/SinInForm';
+import SinUpForm from './Components/SinUpForm'
+function App(props) {
+    
+    const [doHaveAccount,setDoHaveAccount]=useState(false);
+    
+    return (
+        <div>
+            <Grid container>
+                <Grid item lg={1} md={1} sm={0}></Grid> 
+                <Grid item lg={10} md={10} sm={12} >
+                    <Card
+                        elevation={0}
+                        style={{border:lightBorder,marginLeft:'auto',marginRight:'auto',marginTop:'2rem',width:'30rem',height:'35rem'}}
+                    >
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+                        <CardContent>
+                            
+                            {
+                                (!doHaveAccount) && <div>
+                                    <SinUpForm doHaveAccount={doHaveAccount} setDoHaveAccount={setDoHaveAccount}/>
+                                </div>
+                            }
+                            {
+                                (doHaveAccount) && <div>
+                                    <SinInForm  doHaveAccount={doHaveAccount} setDoHaveAccount={setDoHaveAccount}/>
+                                </div>
+                            }
+                        </CardContent>
+                    </Card>
+                </Grid> 
+                <Grid item lg={1} md={1} sm={0}></Grid> 
+            </Grid>
+        </div>
+    );
 }
 
 export default App;
